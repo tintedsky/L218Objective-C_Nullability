df25403f9fecb29b62a9502c130ca8b0228cf656
tintedsky <hongbonew@gmail.com>
2017-10-24 13:52:55 -0400

    Initial Commit

---
+++ L217Objective-C_NullMeBaby/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  L217Objective-C_NullMeBaby
+//
+//  Created by Hongbo Niu on 2017-10-24.
+//  Copyright © 2017 Udemy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
